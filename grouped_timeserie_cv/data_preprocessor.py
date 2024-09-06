7cc18130af9874c6f06c7ccb58324ab205ab933d
ZentaGoran <goran.backlund@zenta.se>
2024-09-06 20:37:06 +0200

    warning if inconsistent_groups

--- grouped_timeserie_cv/data_preprocessor.py
+++ grouped_timeserie_cv/data_preprocessor.py
@@ -8,6 +8,13 @@ class DataPreprocessor:
         data_copy[datetime_column] = pd.to_datetime(data_copy[datetime_column])
         data_copy.set_index(datetime_column, inplace=True)
         groups =   data_copy.index.to_period(frequency).astype(str)
+
+        unique_labels_per_group = data_copy.groupby(groups)[label_column].nunique()
+        inconsistent_groups = unique_labels_per_group[unique_labels_per_group > 1]
+    
+        if not inconsistent_groups.empty:
+          print(f"Warning: {len(inconsistent_groups)} groups have more than one unique label, which may affect the model's performance.")
+    
         X = data.drop(columns=[label_column,  datetime_column])
         y = data[label_column]
     
