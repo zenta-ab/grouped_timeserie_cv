73c5c8165e83eab70bc1bd2b66ff80ef6897d89c
Göran Bäcklund <goran.backlund@zenta.se>
2025-10-11 19:21:08 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grouped_timeserie_cv',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=["plotly", 'pandas', 'scikit-learn', 'matplotlib'],
     author='Zenta AB',
